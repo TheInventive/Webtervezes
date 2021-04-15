d32f64cd3e5f1d0b8433f80ecee738fafb8bea5d
erikhosszu <erikhosszu997@gmail.com>
2021-04-15 17:18:00 +0200

    fix

--- php/register.php
+++ php/register.php
@@ -3,39 +3,34 @@ require_once "User.php";
 require_once "session-start.php";
     $fiokok = User::loadUsers("../users.txt");
 
-    $hibak = [];
-
+    try{
     if (isset($_POST["regis"])) {
-     if (!isset($_POST["username"]) || trim($_POST["username"]) === "")
-      $hibak[] = "A felhasználónév megadása kötelező!";
+        if (!isset($_POST["username"]) || trim($_POST["username"]) === "")
+            throw new Exception("A felhasználónév megadása kötelező!");
 
         if (!isset($_POST["password"]) || trim($_POST["password"]) === "")
-        $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
+            throw new Exception("A jelszó és az ellenőrző jelszó megadása kötelező!");
 
 
         $felhasznalonev = $_POST["username"];
         $jelszo = $_POST["password"];
 
-        if($fiokok != NULL){
-         foreach ($fiokok as $fiok) {
-            if ($fiok["username"] === $felhasznalonev)
-                $hibak[] = "A felhasználónév már foglalt!";
+        if ($fiokok != NULL) {
+            foreach ($fiokok as $fiok) {
+                if ($fiok->getUsername() === $felhasznalonev)
+                    throw new Exception("A felhasználónév már foglalt!");
             }
         }
 
-    if (strlen($jelszo) < 8)
-      $hibak[] = "A jelszónak legalább 8 karakter hosszúnak kell lennie!";
+        if (strlen($jelszo) < 8)
+            throw new Exception("A jelszónak legalább 8 karakter hosszúnak kell lennie!");
 
-    if (count($hibak) === 0) {
-      $fiokok[] = new User($felhasznalonev, $jelszo,
-              $_POST["date-of-birth"],$_POST["sex"], $_POST["email"], $_POST["tel"]);
-      User::saveUsers("../users.txt", $fiokok);
-      $siker = TRUE;
-      header("Location: ../Html/login.php");
-
-    } else {
-      $siker = FALSE;
-      User::saveUsers("users.txt", $hibak);
-      header("Location: index.php");
+        $fiokok[] = new User($felhasznalonev, $jelszo,
+            $_POST["date-of-birth"], $_POST["sex"], $_POST["email"], $_POST["tel"]);
+        User::saveUsers("../users.txt", $fiokok);
+        $siker = TRUE;
+        header("Location: ../Html/login.php");
     }
-  }
+  }catch (Exception $exception){
+        echo $exception->getMessage();
+    }
