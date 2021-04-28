2cee64fa1965ac0b9b981d23407dabbd8801d934
krisz <belovai.k22@gmail.com>
2021-04-28 20:29:04 +0200

    nem működött az előző

--- php/register.php
+++ php/register.php
@@ -29,33 +29,7 @@ require_once "session-start.php";
         if (strlen($felhasznalonev)<6)
            throw new Exception("A felhasználónévnek legalább 6 karakter hosszúnak kell lennie")
 
-        function validate_password( $jelszo ) {
-          $match = '/^[a-z][A-za-z0-9]*$/';
-          if ( preg_match( $match, $jelszo ) ) {
-            return true;
-          } else {
-            return false;
-          };
-        }
-        if( validate_password( $jelszo ) ) {
-          echo 'A jelszó megfelel a követelményeknek';
-        } else {
-          echo 'A jelszónak kisbetűvel kell kezdődnie';
-        };
 
-        function validate_username( $felhasznalonev ) {
-                  $match = '/^[A-Z][A-za-z]{3,6}[0-9]{2}$/';
-                  if ( preg_match( $match, $felhasznalonev ) ) {
-                    return true;
-                  } else {
-                    return false;
-                  };
-                }
-                if( validate_username( $felhasznalonev ) ) {
-                  echo 'A felhasználónév megfelel a követelményeknek';
-                } else {
-                  echo 'A felhasználónévnek nagybetűvel kell kezdődnie és 2 db számmal kell végződnie';
-                };
 
         $fiokok[] = new User($felhasznalonev, $jelszo,
             $_POST["date-of-birth"], $_POST["sex"], $_POST["email"], $_POST["tel"]);
